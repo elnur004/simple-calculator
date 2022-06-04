5dbce8d5ba5ddcd1ba6339d51fd95c630e37d3bb
elnur004 <lnur004@gmail.com>
2022-06-05 01:50:47 +0400

    improved input type to number and cleared input field after input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = currentResult + userInput.value;
+    currentResult = currentResult + parseInt(userInput.value);
     outputResult(currentResult, '');
+    userInput.value = '';
 }
 
 addBtn.addEventListener('click', add);
